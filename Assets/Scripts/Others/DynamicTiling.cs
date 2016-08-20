ba1ba15cfa9d2bb0ec0d1c7d80cf5debb0cc7de0
Dyougi <nightmare0169@hotmail.fr>
2016-08-20 16:22:42 +0200

    Dynamic tiling script added

---
+++ Assets/Scripts/Others/DynamicTiling.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicTiling : MonoBehaviour {
+
+	// UNITY METHODES
+
+	void Start () {
+            GetComponent<Renderer>().material.mainTextureScale = new Vector2(gameObject.transform.localScale.x, gameObject.transform.localScale.y);
+    }
+	
+}
